418e558c383321ee00f7b891a9bc2c1d5f2fd697
JeanElHouwayek <jye12@mail.aub.edu>
2025-06-16 21:29:04 +0300

    second section of the home page is done

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import FirstSection from "./firstSection";
+import SecondSection from "./SecondSection";
 
 const WelcomePage = () => {
   const navigate = useNavigate();
   return (
-    <FirstSection />
+    <>
+      <FirstSection />
+      <SecondSection />
+    </>
   );
 };
 
