c913d2a11f7e601a4ee366d16082e40e454c2f6c
wassimhassan <wassim.7.hassan@gmail.com>
2025-07-17 18:09:00 +0300

    fixing reser password route in app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./components/Signup.js"; // ✅ Your version
 import Login from "./components/Login";
-//import ResetPassword from "./components/ResetPassword.js";
-//import ChangePassword from "./components/ChangePassword.js";
+import ResetPassword from "./components/ResetPassword.js";
+import ChangePassword from "./components/ChangePassword.js";
 import WelcomePage from "./components/WelcomePage";
 import ProfileCard from "./components/ProfileCard.js";
 
@@ -16,8 +16,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<ProfileCard /> } />
-  <Route path="/" element={<WelcomePage />} />
-  <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         {/* Fallback – unknown path */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
